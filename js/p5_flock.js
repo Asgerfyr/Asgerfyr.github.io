448bcc85898c72a2440e8d3d979e0243d0657aee
Asgerfyr <asger.g.stidsen@gmail.com>
2025-05-11 17:31:00 +0200

    p5 header background

--- js/p5_flock.js
+++ js/p5_flock.js
@@ -1,18 +1,52 @@
 let folk_handeler;
+let canvasDiv;
 const functions = {
-  attractAlly: (diffrence,distence2)=>{return {x:(diffrence.x*distence2)/20000000000,y:(diffrence.y*distence2)/20000000000}},
-  attractCenter: (diffrence,distence2)=>{return {x:(diffrence.x*distence2)/7000000,y:(diffrence.y*distence2)/7000000}},
-  repelAlly: (diffrence,distence2)=>{return {x:-(diffrence.x*10)/(distence2/20000)/10000,y:-(diffrence.y*10)/(distence2/20000)/10000}},
-  reactThreat:(diffrence,distence2)=>{return {x:-(diffrence.x*100)/(distence2/200000)/10000,y:-(diffrence.y*100)/(distence2/200000)/10000}}
-}
+  attractAlly: (diffrence, distence2) => {
+    return {
+      x: (diffrence.x * distence2) / (20000000 * width),
+      y: (diffrence.y * distence2) / (20000000 * width),
+    };
+  },
+  attractCenter: (diffrence, distence2) => {
+    return {
+      x: (diffrence.x * distence2) / (7000 * width),
+      y: (diffrence.y * distence2) / (7000 * width),
+    };
+  },
+  repelAlly: (diffrence, distence2) => {
+    return {
+      x: -(diffrence.x * 10) / (distence2 / (20 * width)) / 10000,
+      y: -(diffrence.y * 10) / (distence2 / (20 * width)) / 10000,
+    };
+  },
+  reactThreat: (diffrence, distence2) => {
+    return {
+      x: -(diffrence.x * 100) / (distence2 / (200 * width)) / 10000,
+      y: -(diffrence.y * 100) / (distence2 / (200 * width)) / 10000,
+    };
+  },
+};
 
 function setup() {
-  let canvasDiv = select('#header-canvas');
+  canvasDiv = select("#header-canvas");
   let w = canvasDiv.width;
   let h = canvasDiv.height;
   let cnv = createCanvas(w, h);
   cnv.parent(canvasDiv);
   folk_handeler = new Main();
+  noLoop();
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log("Element is on screen!");
+        loop(); // start drawing
+      } else {
+        noLoop(); // stop drawing
+      }
+    });
+  });
+  observer.observe(canvasDiv.elt);
 }
 
 function draw() {
@@ -21,174 +55,184 @@ function draw() {
   folk_handeler.draw();
 }
 
-class Main{
-  constructor(num_particles_=100,num_threats_ = 3,update_jump_=1){
-    this.particles= [];
-    while (num_particles_--){
+class Main {
+  constructor(num_particles_ = 100, num_threats_ = 3, update_jump_ = 1) {
+    this.particles = [];
+    while (num_particles_--) {
       this.particles.push(new Particle());
     }
-    
+
     this.threats = [];
-    while (num_threats_--){
+    while (num_threats_--) {
       this.threats.push(new Threat());
     }
-    this.update_jump = update_jump_+1;
+    this.update_jump = update_jump_ + 1;
   }
-  
-  updateParticles(){
-    for(let i = 0; frameCount%this.update_jump+i < this.particles.length; i+= this.update_jump){
-      let obj = this.particles[frameCount%this.update_jump+i];
-      for(let j of this.particles){
-        if(obj.pos.x != j.pos.x && obj.pos.y != j.pos.y)obj.allyReact(j.pos);
+
+  updateParticles() {
+    this.update_jump = ceil(this.particles.length / 300);
+    for (
+      let i = 0;
+      (frameCount % this.update_jump) + i < this.particles.length;
+      i += this.update_jump
+    ) {
+      let obj = this.particles[(frameCount % this.update_jump) + i];
+      for (let j of this.particles) {
+        if (obj.pos.x != j.pos.x && obj.pos.y != j.pos.y) obj.allyReact(j.pos);
       }
-      for(let j of this.threats){
-       obj.threatReact(j.pos);
+      for (let j of this.threats) {
+        obj.threatReact(j.pos);
       }
-      if(obj.pos.x != mouseX && obj.pos.y != mouseY)obj.allyReact({x:mouseX,y:mouseY});
+      if (obj.pos.x != mouseX && obj.pos.y != mouseY)
+        obj.threatReact({ x: mouseX, y: mouseY });
       obj.update();
     }
-    
-    for(let i of this.threats){
+
+    for (let i of this.threats) {
       i.update();
       i.draw();
     }
   }
-  
-  draw(){
-    if(mouseIsPressed) this.particles.push(new Particle({x:mouseX,y:mouseY}));
+
+  draw() {
+    if (mouseIsPressed)
+      this.particles.push(new Particle({ x: mouseX, y: mouseY }));
     this.drawParticles();
     this.drawThreats();
   }
-  
-  drawThreats(){
-    for(let i of this.threats){
-     i.draw(); 
+
+  drawThreats() {
+    for (let i of this.threats) {
+      i.draw();
     }
   }
-  
-  drawParticles(){
-    for(let i of this.particles){
-     i.draw(); 
+
+  drawParticles() {
+    for (let i of this.particles) {
+      i.draw();
     }
   }
 }
 
-class Particle{
-  constructor(pos_={"x":random(0,width),"y":random(0,width)},vel_={"x":0,"y":0}){
-    
-    this.pos = {...pos_};
-    this.vel = {...vel_};
-    
+class Particle {
+  constructor(
+    pos_ = { x: random(0, width), y: random(0, width) },
+    vel_ = { x: 0, y: 0 }
+  ) {
+    this.pos = { ...pos_ };
+    this.vel = { ...vel_ };
   }
-  
-  allyReact(allyPos){
-    const diffrence = {x:allyPos.x-this.pos.x, y:allyPos.y-this.pos.y};
-    const distence2 = pow(diffrence.x,2)+pow(diffrence.y,2);
-    const attractVec = functions.attractAlly(diffrence,distence2);
-    const repelVec = functions.repelAlly(diffrence,distence2);
-    const {x,y} = addVec(attractVec,repelVec);
+
+  allyReact(allyPos) {
+    const diffrence = { x: allyPos.x - this.pos.x, y: allyPos.y - this.pos.y };
+    const distence2 = pow(diffrence.x, 2) + pow(diffrence.y, 2);
+    const attractVec = functions.attractAlly(diffrence, distence2);
+    const repelVec = functions.repelAlly(diffrence, distence2);
+    const { x, y } = addVec(attractVec, repelVec);
     this.vel.x += x;
     this.vel.y += y;
   }
-  
-  centerForce(){
-    const diffrence = {x:width/2-this.pos.x, y:height/2-this.pos.y};
-    const distence2 = pow(diffrence.x,2)+pow(diffrence.y,2);
-    const {x,y} = functions.attractCenter(diffrence,distence2);
-    
+
+  centerForce() {
+    const diffrence = { x: width / 2 - this.pos.x, y: height / 2 - this.pos.y };
+    const distence2 = pow(diffrence.x, 2) + pow(diffrence.y, 2);
+    const { x, y } = functions.attractCenter(diffrence, distence2);
+
     this.vel.x += x;
     this.vel.y += y;
   }
-  
-  threatReact(ThreatPos){
-    const diffrence = {x:ThreatPos.x-this.pos.x, y:ThreatPos.y-this.pos.y};
-    const distence2 = pow(diffrence.x,2)+pow(diffrence.y,2);
-    const {x,y} = functions.reactThreat(diffrence,distence2);
+
+  threatReact(ThreatPos) {
+    const diffrence = {
+      x: ThreatPos.x - this.pos.x,
+      y: ThreatPos.y - this.pos.y,
+    };
+    const distence2 = pow(diffrence.x, 2) + pow(diffrence.y, 2);
+    const { x, y } = functions.reactThreat(diffrence, distence2);
     this.vel.x += x;
     this.vel.y += y;
   }
-  
-  drag(){
-    this.vel.x = this.vel.x*(map(folk_handeler.particles.length, 50, 500, 0.99 ,0.9));
-    this.vel.y = this.vel.y*(map(folk_handeler.particles.length, 50, 500, 0.99 ,0.9));
+
+  drag() {
+    this.vel.x =
+      this.vel.x * map(folk_handeler.particles.length, 50, 500, 0.99, 0.9);
+    this.vel.y =
+      this.vel.y * map(folk_handeler.particles.length, 50, 500, 0.99, 0.9);
   }
-  
-  update(){
+
+  update() {
     this.centerForce();
     this.drag();
     this.constrain();
-    this.pos.x += this.vel.x
-    this.pos.y += this.vel.y
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
   }
-  
-  constrain(){
-    if(this.pos.x < 0 && this.pos.x > width){
-      this.pos.x = constrain(this.pos.x,0,width);
+
+  constrain() {
+    if (this.pos.x < 0 && this.pos.x > width) {
+      this.pos.x = constrain(this.pos.x, 0, width);
       this.vel.x = 0;
     }
-    if(this.pos.y < 0 && this.pos.y > height){
-      this.pos.y = constrain(this.pos.y,0,height);
+    if (this.pos.y < 0 && this.pos.y > height) {
+      this.pos.y = constrain(this.pos.y, 0, height);
       this.vel.y = 0;
     }
-    
-    this.vel.x = constrain(this.vel.x,-20,20);
-    this.vel.y = constrain(this.vel.y,-20,20);
+
+    this.vel.x = constrain(this.vel.x, -20, 20);
+    this.vel.y = constrain(this.vel.y, -20, 20);
   }
-  
-  draw(){
+
+  draw() {
     push();
-      stroke("blue")
-      strokeWeight(2);
-      translate(this.pos.x,this.pos.y);
-      line(0,0,this.vel.x,this.vel.y)
+    stroke("blue");
+    strokeWeight(2);
+    translate(this.pos.x, this.pos.y);
+    line(0, 0, this.vel.x, this.vel.y);
     pop();
   }
-  
 }
 
+class Threat {
+  constructor(
+    pos_ = { x: random(0, width), y: random(0, width) },
+    vel_ = { x: 0, y: 0 }
+  ) {
+    this.pos = { ...pos_ };
+    this.vel = { ...vel_ };
+  }
 
-class Threat{
-  constructor(pos_={"x":random(0,width),"y":random(0,width)},vel_={"x":0,"y":0}){
-    
-    this.pos = {...pos_};
-    this.vel = {...vel_};
-    
+  drag() {
+    this.vel.x = this.vel.x * 0.99;
+    this.vel.y = this.vel.y * 0.99;
   }
-  
-  
-  drag(){
-    this.vel.x = this.vel.x*0.99;
-    this.vel.y = this.vel.y*0.99;
-  }
-  
-  update(){
-    this.vel.x += random(-0.5,0.5);
-    this.vel.y += random(-0.5,0.5);
+
+  update() {
+    this.vel.x += random(-0.5, 0.5);
+    this.vel.y += random(-0.5, 0.5);
     this.drag();
     this.constrain();
-    this.pos.x += this.vel.x
-    this.pos.y += this.vel.y
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
   }
-  
-  constrain(){
-    this.pos.x = constrain(this.pos.x,0,width);
-    this.pos.y = constrain(this.pos.y,0,height);
-    
-    this.vel.x = constrain(this.vel.x,-100,100);
-    this.vel.y = constrain(this.vel.y,-100,100);
+
+  constrain() {
+    this.pos.x = constrain(this.pos.x, 0, width);
+    this.pos.y = constrain(this.pos.y, 0, height);
+
+    this.vel.x = constrain(this.vel.x, -10, 10);
+    this.vel.y = constrain(this.vel.y, -10, 10);
   }
-  
-  draw(){
+
+  draw() {
     push();
-      stroke("red")
-      strokeWeight(10);
-      translate(this.pos.x,this.pos.y);
-      line(0,0,this.vel.x,this.vel.y)
+    stroke("red");
+    strokeWeight(10);
+    translate(this.pos.x, this.pos.y);
+    line(0, 0, this.vel.x, this.vel.y);
     pop();
   }
 }
 
-function addVec(vec1,vec2){
-  return {x:vec1.x += vec2.x,
-          y:vec1.y += vec2.y}
+function addVec(vec1, vec2) {
+  return { x: (vec1.x += vec2.x), y: (vec1.y += vec2.y) };
 }
