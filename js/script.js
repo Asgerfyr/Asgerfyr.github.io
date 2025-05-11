9389214901c1f30c9edaa90c5d9ada2af61bbaa6
Asgerfyr <asger.g.stidsen@gmail.com>
2025-05-11 12:37:42 +0200

    qouts typewriter and fix multy tipewriter

--- js/script.js
+++ js/script.js
@@ -102,7 +102,7 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 
 //typewriter effect
 const tryperContainers = document.querySelectorAll(".js-tryper-container");
-let trypers = {};
+let trypers = new Map();
 const curserBlinkAfter = 500;
 
 tryperContainers.forEach((container) => {
@@ -110,20 +110,20 @@ tryperContainers.forEach((container) => {
   const curser = container.querySelector(".js-curser");
   const data = JSON.parse(tryper.dataset.info);
 
-  trypers[container] = {
+  trypers.set(container, {
     DOM_typer: tryper,
     DOM_curser: curser,
     currentStringData: { index: 0, charIndex: 0 },
     ...data,
-  };
+  });
 
-  typewriterNextOperation(container);
+  return typewriterNextOperation(container);
 });
 
 function typewriterNextOperation(container) {
-  const { currentStringData, strings } = trypers[container];
+  const { currentStringData, strings } = trypers.get(container);
   writeMode = strings[currentStringData.index].typeStyle;
-  typewriterRemoveBlinkCurser(trypers[container].DOM_curser);
+  typewriterRemoveBlinkCurser(trypers.get(container).DOM_curser);
   switch (writeMode) {
     case "blink":
       typewriterBlink(container);
@@ -139,7 +139,7 @@ function typewriterNextOperation(container) {
 
 function typewriterAddChar(container) {
   const { currentStringData, strings, DOM_typer, typeSpeed } =
-    trypers[container];
+    trypers.get(container);
 
   // get char to add
   const currentString = strings[currentStringData.index].string;
@@ -166,7 +166,7 @@ function typewriterAddChar(container) {
 }
 
 function typewriterBlink(container) {
-  const { currentStringData, strings, DOM_typer } = trypers[container];
+  const { currentStringData, strings, DOM_typer } = trypers.get(container);
   const currentString = strings[currentStringData.index].string;
 
   DOM_typer.innerHTML = currentString;
@@ -180,23 +180,13 @@ function typewriterBlink(container) {
 
 function typewriterDeleteTo(container) {
   const { currentStringData, strings, DOM_typer, typeSpeed } =
-    trypers[container];
+    trypers.get(container);
 
   // get current string to delete to
   const currentString = strings[currentStringData.index].string;
-
+  
   // delete a char in the string
   DOM_typer.innerHTML = DOM_typer.innerHTML.slice(0, -1);
-
-  console.log(
-    `element: ${DOM_typer.innerHTML} string: ${currentString.slice(
-      0,
-      DOM_typer.innerHTML.length
-    )} result: ${
-      DOM_typer.innerHTML != currentString.slice(0, DOM_typer.innerHTML.length)
-    }`
-  );
-
   // if the object dosent match the object to delete to
   if (
     DOM_typer.innerHTML.length > 0 &&
@@ -206,7 +196,7 @@ function typewriterDeleteTo(container) {
       typewriterDeleteTo(container);
     }, typeSpeed);
   }
-  console.log(`string data: ${currentString}`);
+
   return typewriterCleanupPause(
     container,
     strings,
@@ -215,11 +205,11 @@ function typewriterDeleteTo(container) {
 }
 
 function typewriterCleanupPause(container, strings, pauseFor = 0) {
-  const { currentStringData } = trypers[container];
+  const { currentStringData } = trypers.get(container);
   currentStringData.charIndex = 0;
   currentStringData.index++;
   if (currentStringData.index >= strings.length) {
-    if (!trypers[container].loop) return;
+    if (!trypers.get(container).loop) return;
     currentStringData.index = 0;
   }
 
@@ -242,6 +232,6 @@ function typewriterRemoveBlinkCurser(curser) {
 }
 
 function typewriterAddBlinkCurser(container) {
-  const { DOM_curser } = trypers[container];
+  const { DOM_curser } = trypers.get(container);
   DOM_curser.classList.add("js-blinkcurser");
 }
